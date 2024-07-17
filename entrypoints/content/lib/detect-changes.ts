ae46946141e4a81ce933ab05e93e07fbf39b7a11
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2024-07-17 17:32:08 +0800

    feat: card contents via vue

--- entrypoints/content/lib/detect-changes.ts
+++ entrypoints/content/lib/detect-changes.ts
@@ -1,3 +1,5 @@
+import TweetCard from "@/entrypoints/content/components/tweet-card.vue";
+
 export function detectChanges() {
   const timelineDiv = document.querySelector('div.css-175oi2r.r-f8sm7e.r-13qz1uu.r-1ye8kvj > section > div.css-175oi2r');
   if (!timelineDiv) {
@@ -14,11 +16,14 @@ export function detectChanges() {
     if (!articleParentElement) {
       return;
     }
-    const talkElement = articleCoreContent?.cloneNode(true);
-    (talkElement as Element)?.classList.add('tweet-talk');
+    // const talkElement = articleCoreContent?.cloneNode(true);
     if (articleParentElement.querySelector('.tweet-talk')) {
       return;
     }
+    const talkElement = document.createElement('div');
+    talkElement.setAttribute('class', 'css-175oi2r r-18u37iz tweet-talk');
+    const app = createApp(TweetCard);
+    app.mount(talkElement);
     articleParentElement.append(talkElement);
   })
 }
